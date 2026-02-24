357e46198c48dd9bec20dd7e76d7cc252015b94c
Matheus Cruz Albernaz <matheusalbernaz2003@gmail.com>
2026-02-24 00:06:12 -0300

    Curso APIs com Python - 1. Django REST

---
+++ Alura/Trilha - Back-End Python do zero ao avançado/3. Django REST Framework crie APIs com Python/1. Trabalhando com Django REST/1.1 Django REST Framework construindo APIs RESTful do Zero/ESCOLA/escola/views.py
@@ -0,0 +1,41 @@
+# from django.http import JsonResponse
+
+# def estudantes(request):
+#     if request.method == 'GET':
+#         estudante = {
+#             'id': '1',
+#             'nome': 'Matheus'
+#         }
+#         return JsonResponse(estudante)
+
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
+from rest_framework import viewsets, generics
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudanteGeneric(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCursoGeneric(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
