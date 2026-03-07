da3bdfa3ee63baefb4d46283c201d511158ea7aa
Matheus Cruz Albernaz <matheusalbernaz2003@gmail.com>
2026-03-07 20:30:01 -0300

    Curso Django REST Framework: crie APIs com Python

---
+++ Alura/Trilha - Back-End Python do zero ao avançado/3. Django REST Framework crie APIs com Python/3. Trabalhando com Testes/backend/escola/views.py
@@ -0,0 +1,106 @@
+# from django.http import JsonResponse
+
+# def estudantes(request):
+#     if request.method == 'GET':
+#         estudante = {
+#             'id': '1',
+#             'nome': 'Matheus'
+#         }
+#         return JsonResponse(estudante)
+
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer, EstudanteSerializerV2
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.throttling import UserRateThrottle
+from escola.throttles import MatriculaAnonRateThrottle
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de estudantes.
+
+    Campos de ordenação:
+    - nome: permite ordenar os resultados por nome.
+
+    Campos de pesquisa:
+    - nome: permite pesquisar os resultados por nome.
+    - cpf: permite pesquisar os resultados por CPF.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+
+    Classe de Serializer:
+    - EstudanteSerializer: usado para serialização e desserialização de dados.
+    - Se a versão da API for 'v2', usa EstudanteSerializerV2.
+    """
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    queryset = Estudante.objects.all().order_by("id")
+    # queryset = Estudante.objects.all()
+    # queryset = Estudante.objects.all().order_by('nome')
+    # serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
+
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return EstudanteSerializerV2
+        return EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de cursos.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+    """
+    queryset = Curso.objects.all().order_by("id")
+    serializer_class = CursoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de matrículas.
+
+    Métodos HTTP Permitidos:
+    - GET, POST
+
+    Throttle Classes:
+    - MatriculaAnonRateThrottle: limite de taxa para usuários anônimos.
+    - UserRateThrottle: limite de taxa para usuários autenticados.
+    """
+    queryset = Matricula.objects.all().order_by("id")
+    serializer_class = MatriculaSerializer
+    throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
+    http_method_names = ["get", "post"]
+
+class ListaMatriculaEstudanteGeneric(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk']).order_by("id")
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCursoGeneric(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Curso
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk']).order_by("id")
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
